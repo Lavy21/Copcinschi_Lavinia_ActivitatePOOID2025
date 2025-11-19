f294d3b3f79151b6fe5e4e9766c3cced6fd61cc4
Lavy21 <copcinschilavinia22@stud.ase.ro>
2025-11-19 14:06:57 +0200

    update

--- Source.cpp
+++ Source.cpp
@@ -8,13 +8,56 @@ class Magazin
 	int nrAngajati;
 	float suprafataMagazinMP;
 	bool esteNonStop;
+	const int CUI;
+	static int clasaCaenPrincipala;
 
-	Magazin(string denumireMagazin, int nrAngajati, float suprafataMagazinMP, bool esteNonStop)
+public:
+
+	Magazin():CUI(0)
+	{
+		this->denumireMagazin = "Necunoscut";
+		this->nrAngajati = 0;
+		this->suprafataMagazinMP = 0;
+		this->esteNonStop = 1;
+	}
+
+	Magazin(string denumireMagazin) :CUI(0)
+	{
+		this->denumireMagazin = denumireMagazin;
+		this->nrAngajati = 0;
+		this->suprafataMagazinMP = 0;
+		this->esteNonStop = 1;
+	}
+
+	Magazin(string denumireMagazin, int nrAngajati, float suprafataMagazinMP, bool esteNonStop): CUI(0)
 	{
 		this->denumireMagazin = denumireMagazin;
 		this->nrAngajati = nrAngajati;
 		this->suprafataMagazinMP = suprafataMagazinMP;
 		this->esteNonStop = esteNonStop;
+	}
+
+	string getDenumireMagazin() {
+		return this->denumireMagazin;
+	}
+	int getNrAngajati() {
+		return this->nrAngajati;
+	}
+	float getSuprafataMagazinMP() {
+		this->suprafataMagazinMP;
+	}
+	bool getEsteNonStop() {
+		return this->esteNonStop;
+	}
+	int getCUI() {
+		return this->CUI;
+	}
+
+	void setDenumireMagazin(string denumireNouaMagazin) {
+		this->denumireMagazin = denumireNouaMagazin;
+	}
+	void setnrAngajati(int nrAngajatiNou) {
+		this->nrAngajati = nrAngajatiNou;
 	}
 };
 
